ee8ab88a9470767e0910fd3fd1af41a6eece4131
Ryna Truong <rynatruong@outlook.com>
2020-08-27 17:29:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,21 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    Show.highest_rating
+    Show.order(rating: :desc).limit(1)[0]
+  end
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  
+  def self.least_popular_show
+    Show.irder(rating: :asc_.limit(1)[0]
+  end
+  
+  def self.popular_shows
+    Show.where("rating > 5")
+  end
+  
+  def self.showd_by_alphabetical_order
+    Show.order(name: :asc)
   end
 end
